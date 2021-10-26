36a4f417031e63bab23cd9385e28dd69a21c9570
skperez <stepheny.k.perez@jpl.nasa.gov>
2021-10-26 16:00:00 -0700

    Pull from secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+venue=$1
+version=$2
+file=$3
+provider=$4
+
+
+echo "venue $venue"
+echo "version $version"
+echo "file $file"
+echo "provider $provider"
+echo "cmr_user $cmr_user"
+echo "cmr_pass $cmr_pass"
 time=$(date)
 echo "::set-output name=time::$time"
+
+# umms_updater -d -f cmr/${file} -a cmr/${venue}_associations.txt -p ${provider} -e ${venue} -cu "$cmr_user" -cp "$cmr_pass"
