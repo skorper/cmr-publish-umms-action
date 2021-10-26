b5b876a86fcb3b2852853f84413e74bd7ffa75ef
skperez <stepheny.k.perez@jpl.nasa.gov>
2021-10-26 16:23:23 -0700

    bump

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,18 +14,18 @@ echo "file ${file}"
 echo "provider $provider"
 echo "cmr_user $cmr_user"
 echo "cmr_pass $cmr_pass"
-echo "assoc cmr/${venue}_associations.txt"
+echo "assoc cmr/${env}_associations.txt"
 
 
 # Replace version placeholder with actual version
 jq --arg a $version '.Version = $a' $file > "cmr/cmr.json"
 
-# Replace Harmony URL placeholder with Harmony URL based on venue
-if [[ $tf_venue == "sit" || $tf_venue == "uat" ]]; then
+# Replace Harmony URL placeholder with Harmony URL based on env
+if [[ $env == "sit" || $env == "uat" ]]; then
   jq --arg a "https://harmony.uat.earthdata.nasa.gov" '.URL.URLValue = $a' cmr/cmr.json > cmr/cmr.json.tmp && mv cmr/cmr.json.tmp cmr/cmr.json
 fi
 
-echo "umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p POCLOUD -e ${tf_venue} -cu $cmr_user -cp $cmr_pass"
+echo "umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p POCLOUD -e ${env} -cu $cmr_user -cp $cmr_pass"
 
 echo `cat cmr/cmr.json`
 
