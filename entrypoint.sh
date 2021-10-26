153b2f7cd140764d0a3e48caec29a67838dd229d
skperez <stepheny.k.perez@jpl.nasa.gov>
2021-10-26 16:05:45 -0700

    Bump

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,19 @@
 #!/bin/sh -l
 
-venue=$1
+env=$1
 version=$2
 file=$3
 provider=$4
 
 
-echo "venue $venue"
+echo "env $env"
 echo "version $version"
-echo "file $file"
+echo "file cmr/${file}"
 echo "provider $provider"
 echo "cmr_user $cmr_user"
 echo "cmr_pass $cmr_pass"
+echo "assoc cmr/${venue}_associations.txt"
 time=$(date)
 echo "::set-output name=time::$time"
 
-# umms_updater -d -f cmr/${file} -a cmr/${venue}_associations.txt -p ${provider} -e ${venue} -cu "$cmr_user" -cp "$cmr_pass"
+# umms_updater -d -f cmr/${file} -a cmr/${env}_associations.txt -p ${provider} -e ${env} -cu "$cmr_user" -cp "$cmr_pass"
