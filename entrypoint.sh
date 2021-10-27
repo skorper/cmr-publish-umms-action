4b5ba7253eeb797f917128ab2e8ecac4075fe206
skperez <stepheny.k.perez@jpl.nasa.gov>
2021-10-27 10:21:40 -0700

    action

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set +x
 echo $GITHUB_WORKSPACE
 ls "$GITHUB_WORKSPACE/*"
 
-file="/$1"
+file=$1
 provider=$2
 env=$3
 version=$4
@@ -17,19 +17,19 @@ echo "file ${file}"
 echo "provider $provider"
 echo "cmr_user $cmr_user"
 echo "cmr_pass $cmr_pass"
-echo "assoc /cmr/${env}_associations.txt"
+echo "assoc cmr/${env}_associations.txt"
 
 
 # Replace version placeholder with actual version
-jq --arg a $version '.Version = $a' $file > "/cmr/cmr.json"
+jq --arg a $version '.Version = $a' $file > "cmr/cmr.json"
 
 # Replace Harmony URL placeholder with Harmony URL based on env
 if [[ $env == "sit" || $env == "uat" ]]; then
-  jq --arg a "https://harmony.uat.earthdata.nasa.gov" '.URL.URLValue = $a' /cmr/cmr.json > /cmr/cmr.json.tmp && mv /cmr/cmr.json.tmp /cmr/cmr.json
+  jq --arg a "https://harmony.uat.earthdata.nasa.gov" '.URL.URLValue = $a' cmr/cmr.json > cmr/cmr.json.tmp && mv cmr/cmr.json.tmp /cmr/cmr.json
 fi
 
-echo "umms_updater -d -f /cmr/cmr.json -a /cmr/${env}_associations.txt -p POCLOUD -e ${env} -cu $cmr_user -cp $cmr_pass"
+echo "umms_updater -d -f cmr/cmr.json -a cmr/${env}_associations.txt -p POCLOUD -e ${env} -cu $cmr_user -cp $cmr_pass"
 
-echo `cat /cmr/cmr.json`
+echo `cat cmr/cmr.json`
 
 set -x
